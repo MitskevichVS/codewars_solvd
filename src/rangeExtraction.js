5565250f04c7a96d078cee7f5b3ed829fb5e332c
Victor Mitskevich <mv2.mx@yandex.ru>
2019-12-14 12:26:34 +0300

    fix: formatting

--- src/rangeExtraction.js
+++ src/rangeExtraction.js
@@ -1,23 +1,23 @@
-function solution(list){
+function solution(list) {
 
   const findInterval = (arr, element, start) => {
     let endIndex = start + 1;
     let startElement = element;
     let intermediateElement = arr[endIndex];
-  
+
     while (startElement + 1 === intermediateElement) {
       startElement = intermediateElement;
       endIndex += 1;
       intermediateElement = arr[endIndex];
     };
-    
+
     endIndex -= 1;
     const endElement = arr[endIndex];
     return [endIndex, element, endElement];
   };
 
   let newArr = [];
-  for (let index = 0; index < list.length; index += 1){
+  for (let index = 0; index < list.length; index += 1) {
     if (list[index] + 2 === list[index + 2]) {
       const slicedInterval = findInterval(list, list[index], index);
       index = slicedInterval[0];
