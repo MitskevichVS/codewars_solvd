29098d44dd4966f07ae7d67c908d14ec70d417b8
Victor Mitskevich <mv2.mx@yandex.ru>
2019-12-16 13:16:33 +0300

    fix: check condition, add comments

--- src/sudokuCheck.js
+++ src/sudokuCheck.js
@@ -1,19 +1,42 @@
 function doneOrNot(board) {
   const sudokuMatrix = board;
   let flag = true;
+
+  // Validate row function:
   const checkRows = (matrix) => {
+    // summ function:
     const reducer = (acc, value) => acc + value;
+    // check sorted row function:
+    const checkSortedRow = (row) => {
+      let sortedRowFlag = true;
+      row.forEach((item, index) => {
+        if (item !== index + 1) {
+          sortedRowFlag = false;
+        }
+      });
+      return sortedRowFlag;
+    };
+
+    // Main checkRows cycle:
     matrix.forEach((item) => {
+      // Calculate summ
       const summ = item.reduce(reducer);
-      if (summ !== 45) {
+      // Create new array from row:
+      let sortedItem = Array.from(item);
+      sortedItem = sortedItem.sort();
+      // Main checkRows condition
+      if (summ !== 45 && !checkSortedRow(sortedItem)) {
         flag = false;
       }
     });
     return flag;
   };
 
+  // Check columns function:
   const checkCols = (matrix) => {
+    // Create new matrix:
     let transMatrix = [];
+    // Main checkCols cycle(transpose matrix to use checkRows function):
     matrix.forEach((item) => {
       item.forEach((subItem, subIndex) => {
         if (!transMatrix[subIndex]) {
@@ -22,12 +45,15 @@ function doneOrNot(board) {
         transMatrix[subIndex].push(subItem);
       });
     });
+    // Main checkCols condition
     if (checkRows(transMatrix)) {
       return true;
     } return false;
   };
 
+  // check regions function:
   const checkRegions = (matrix) => {
+    // create new matrix:
     let cols = [];
     let grid = [];
 
@@ -36,6 +62,7 @@ function doneOrNot(board) {
       grid.push([]);
     });
 
+    // main checkRegions cycle (create rows from regions to use checkRows function)
     matrix.forEach((item, index) => {
       item.forEach((subItem, subIndex) => {
         cols[subIndex][index] = subItem;
@@ -47,11 +74,13 @@ function doneOrNot(board) {
       });
     });
 
+    // Main checkRegions condition:
     if (checkRows(grid)) {
       return true;
     } return false;
   };
 
+  // main doneOrNot condition:
   if (checkRows(sudokuMatrix) && checkCols(sudokuMatrix) && checkRegions(sudokuMatrix)) {
     return 'Finished!';
   } return 'Try again!';
